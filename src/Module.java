3a64c75b235f05cf0e709a6825263a732d784c53
ThibakarSri <thibakarsri33@gmail.com>
2025-04-11 15:03:17 +0530

    finish and update the all class files

---
+++ src/Module.java
@@ -0,0 +1,14 @@
+public class Module {
+    private int moduleMarks;
+
+    // Getter and setter
+    public int getModuleMarks()
+    {
+        return moduleMarks;
+    }
+
+    public void setModuleMarks(int moduleMarks)
+    {
+        this.moduleMarks = moduleMarks;
+    }
+}
